a1f41c5f81ab4b56b15448164617eaca057cd99f
HiteshGaikwad <116432379+HiteshGaikwad@users.noreply.github.com>
2023-05-21 18:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let n= str.length;
+	int start=0, end=n-1;
+	while(start<=end){
+		if(str.charAt(start)!==str.charAt(end)){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
